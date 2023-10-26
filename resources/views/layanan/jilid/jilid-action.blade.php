69263f77c9bccf612d99182be511310cb3b56f3d
MuhammadWildanf <wildanf.daftar@gmail.com>
2023-10-26 23:12:35 +0700

    harga otomatis

--- resources/views/layanan/jilid/jilid-action.blade.php
+++ resources/views/layanan/jilid/jilid-action.blade.php
@@ -68,3 +68,44 @@
         </div>
     </form>
 </div>
+
+<script>
+    
+    function updateCalculations() {
+    const pageBerwarna = parseFloat(document.getElementById("page_berwarna").value);
+    const pageHitamPutih = parseFloat(document.getElementById("page_hitamPutih").value);
+    const cover = parseFloat(document.getElementById("cover").value);
+    const exemplar = parseFloat(document.getElementById("exemplar").value);
+
+    if (!isNaN(pageBerwarna) && !isNaN(pageHitamPutih) && !isNaN(cover) && !isNaN(exemplar)) {
+        //ini mengambil dari table price 
+
+        $.ajax({
+            url: '{{ url('konfigurasi/getPriceData') }}',
+            method: 'GET',
+            success: function(response) {
+                const data = response.data[0]; // Mengambil data dari respons
+                const totalPageBerwarna = pageBerwarna * data.pageBerwarnaPrice;
+                const totalPageHitamPutih = pageHitamPutih * data.pageHitamPutihPrice;
+                const totalCover = cover * data.coverprice;
+                const totalPerjilid = exemplar * data.perjilidprice;
+                const total = (totalPageBerwarna + totalPageHitamPutih + totalCover + totalPerjilid);
+
+                document.getElementById("total").value = total;
+            },
+            error: function(xhr, status, error) {
+                console.log('ini Error', error);
+            }
+        });
+    }
+}
+
+// Attach the calculation function to input change events
+document.getElementById("page_berwarna").addEventListener("input", updateCalculations);
+document.getElementById("page_hitamPutih").addEventListener("input", updateCalculations);
+document.getElementById("exemplar").addEventListener("input", updateCalculations);
+document.getElementById("cover").addEventListener("input", updateCalculations);
+
+// Calculate initial values
+updateCalculations();
+</script>
