c3e49e2e3b870f0694675628cd9e1b35047732b8
MuhammadWildanf <wildanf.daftar@gmail.com>
2024-01-17 00:10:02 +0700

    fix edit berkas ta

--- app/Http/Controllers/mahasiswa/FileController.php
+++ app/Http/Controllers/mahasiswa/FileController.php
@@ -90,8 +90,9 @@ class FileController extends Controller
      */
     public function edit(File $berka)
     {
+        $categories = Category::all();
         $documents = Document::all();
-        return view('layanan.mahasiswa.file.file-action', compact('documents', 'berka'));
+        return view('layanan.mahasiswa.file.file-action', compact('categories','documents', 'berka'));
     }
 
     /**
