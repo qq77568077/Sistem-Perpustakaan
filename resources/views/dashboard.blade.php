22c4b179d43d4e63ecf0ed626ffc09b8e7c09d62
MuhammadWildanf <wildanf.daftar@gmail.com>
2024-05-16 09:13:24 +0700

    fix term

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,55 +9,20 @@
         <div class="title">
             Dashboard
         </div>
-        <div class="card">
-            <div class="card-header">
-                Persyaratan dan Ketentuan Cek Plagiarism
-            </div>
-            <div class="card-body">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et libero sed mauris dignissim tincidunt.
-                    Sed vitae arcu lectus. Integer pharetra ultricies purus non volutpat.
-                </p>
-                <p>
-                    In hac habitasse platea dictumst. Sed pulvinar nisi eu felis suscipit, nec pretium risus pretium.
-                    Integer et orci eu enim ultrices consequat.
-                </p>
-                <!-- Tambahkan konten persyaratan dan ketentuan di sini -->
-            </div>
+        <div class="content-wrapper">
+            @foreach ($data as $item)
+                <div class="card mb-3">
+                    <div class="card-header">
+                        {{ $item->nama }}
+                    </div>
+                    <div class="card-body">
+                        <p>
+                            {!! $item->syarat !!}
+                        </p>
+                    </div>
+                </div>
+            @endforeach
         </div>
-        <div class="card">
-            <div class="card-header">
-                Persyaratan dan Ketentuan Jilid
-            </div>
-            <div class="card-body">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et libero sed mauris dignissim tincidunt.
-                    Sed vitae arcu lectus. Integer pharetra ultricies purus non volutpat.
-                </p>
-                <p>
-                    In hac habitasse platea dictumst. Sed pulvinar nisi eu felis suscipit, nec pretium risus pretium.
-                    Integer et orci eu enim ultrices consequat.
-                </p>
-                <!-- Tambahkan konten persyaratan dan ketentuan di sini -->
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-header">
-                Persyaratan dan Ketentuan Pengumpulan Berkas TA
-            </div>
-            <div class="card-body">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et libero sed mauris dignissim tincidunt.
-                    Sed vitae arcu lectus. Integer pharetra ultricies purus non volutpat.
-                </p>
-                <p>
-                    In hac habitasse platea dictumst. Sed pulvinar nisi eu felis suscipit, nec pretium risus pretium.
-                    Integer et orci eu enim ultrices consequat.
-                </p>
-                <!-- Tambahkan konten persyaratan dan ketentuan di sini -->
-            </div>
-        </div>
-
     </div>
 @endsection
 
